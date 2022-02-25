d6ad776e6433970e3db68270f3a0a2d8c046854b
Maximo Oen <mmo2134@columbia.edu>
2022-02-24 21:24:30 -0500

    Added gaussian blur

--- catkin_ws/src/mapping/costmap/scripts/costmap_node.py
+++ catkin_ws/src/mapping/costmap/scripts/costmap_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import copy
+from cv2 import blur
 import numpy as np
 import threading
 import math
@@ -9,12 +10,13 @@ from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
 import time
 import rospy
+import cv2
 
 # Useful constants.
 # TODO: Create library for project-wide constants such as these
 class Constants:
-    sigma = 0
-    kernel_size = 0
+    sigma = 10
+    kernel_size = 13
 
 # Wrapper to synchronize access to OccupancyGrid costmap
 class State:
@@ -25,12 +27,23 @@ class State:
     def update(self, newGridmap, mutex=None):
         # Do calculations on newGridmap here
 
+        # int8[]--> 2-D CostMap
+        #data = np.array(newGridmap.data).reshape((newGridmap.info.height, newGridmap.info.width))
+
+        # Gaussian Blur
+        blurred_map = cv2.GaussianBlur(np.array(newGridmap.data), (Constants.kernel_size,Constants.kernel_size), Constants.sigma)
+
+        # Return it back from 2-D np array to a int8[] 
+        newGridmap.data = blurred_map
 
         # Reading variables must be done synchronously to avoid race conditions
         if (mutex): mutex.acquire()
+        
         # Update costmap here
-
-
+        # Update the costmap only if the timestamp is later and both have equal frame_id
+        #if self.costmap.header.stamp < newGridmap.header.stamp and self.costmap.header.frame_id == newGridmap.header.frame_id:
+        self.costmap = newGridmap
+        
         if (mutex): mutex.release()
 
     # Returns costmap object to be broadcast to rostopic. Pass a mutex to make thread-safe.
@@ -50,7 +63,7 @@ class CostmapNode:
         self.state = State()
         self.mutex = threading.Lock()
 
-        self.publisher = rospy.Publisher('/localization/dead_reckon/odom', Odometry, queue_size=10, latch=True)
+        self.publisher = rospy.Publisher('/mapping/cost-map', Odometry, queue_size=10, latch=True)
         self.subscriber = rospy.Subscriber('/map', OccupancyGrid, self.grid_map_callback, queue_size=1)
         
 
