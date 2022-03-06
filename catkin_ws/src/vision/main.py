a8b025d8850294800ac626222bea907d028e8ca3
Stephen Petrides <sdpetrides@gmail.com>
2022-03-06 18:27:30 -0500

    Add open3d plane detection demo via upload

---
+++ catkin_ws/src/vision/main.py
@@ -0,0 +1,39 @@
+import open3d as o3d
+import numpy as np
+
+if __name__ == "__main__":
+
+    # Source: http://www.cvlibs.net/datasets/kitti-360/index.php
+    filename = "./input_clouds/0000003503_0000003724.ply"
+
+    # Read PLY (Point Cloud)
+    pcd = o3d.io.read_point_cloud(filename)
+    pcd = pcd.voxel_down_sample(voxel_size=0.5)
+
+    pcd = o3d.geometry.PointCloud(
+        points=o3d.cpu.pybind.utility.Vector3dVector(
+            np.asarray(
+                pcd.points
+            )
+        )
+    )
+
+    # Use RANSAC to segment PC into ground plane
+    plane_model, inliers = pcd.segment_plane(
+        distance_threshold=0.15,
+        ransac_n=3,
+        num_iterations=1000
+    )
+
+    # Create new PCDs (one for ground and one for not-ground)
+    inlier_cloud = pcd.select_by_index(inliers)
+    outlier_cloud = pcd.select_by_index(inliers, invert=True)
+    inlier_cloud.paint_uniform_color([1.0, 0, 0])
+
+    # Write to PLY files
+    try:
+        o3d.io.write_point_cloud("./output_clouds/inlier_cloud_1.ply", inlier_cloud)
+        o3d.io.write_point_cloud("./output_clouds/outlier_cloud_1.ply", outlier_cloud)
+    except Exception as e:
+        print("Something went wrong with writing PLY output files.")
+        raise e
