12d29c10cee124e0c816d92828b4f2d17c5da3cc
NeilNie <yongyang.nie@gmail.com>
2022-03-10 21:33:28 -0500

    change steering joystick control

--- catkin_ws/src/controls/scripts/teleop-pub-sub.py
+++ catkin_ws/src/controls/scripts/teleop-pub-sub.py
@@ -7,29 +7,25 @@ class TeleOpNode():
 
     def callback(self, data):
         speed = data.axes[1] # Left stick vertical axis
-        angle = data.axes[0] # Left stick horizontal axis
+        angle = data.axes[2] # Left stick horizontal axis
 
-        speed_to_rad = ((speed)*(250))
-        speed_to_rad = min(250, speed_to_rad)
+        speed_to_rad = ((speed)*(500))
+        speed_to_rad = min(500, speed_to_rad)
         
-        angle = (angle+1)/2
+        angle = (-1 * angle+1)/2
         
         self.speed_to_pub = speed_to_rad
         self.angle_to_pub = angle
 
-        print(speed_to_pub, speed_to_rad)
-
     def __init__(self):
-        global speed_topic
-        global steer_angle_topic
     
         self.speed_to_pub = 0
         self.angle_to_pub = 0.5
 
-        speed_topic = rospy.Publisher("systems/output/speed", Float64)
-        steer_angle_topic = rospy.Publisher("systems/output/steer_angle", Float64)
+        self.speed_topic = rospy.Publisher("systems/output/speed", Float64)
+        self.steer_angle_topic = rospy.Publisher("systems/output/steer_angle", Float64)
         
-        rospy.Subscriber("systems/input/joystick", Joy, self.callback)
+        rospy.Subscriber("/joy", Joy, self.callback)
         
         rospy.init_node("teleop_node")
     
@@ -37,10 +33,11 @@ class TeleOpNode():
     
         while not rospy.is_shutdown():
             print("in while loop")
-            speed_topic.publish(self.speed_to_pub)
-            steer_angle_topic.publish(self.angle_to_pub)
+            self.speed_topic.publish(self.speed_to_pub)
+            self.steer_angle_topic.publish(self.angle_to_pub)
             print("speed: " + str(self.speed_to_pub))
             rate.sleep()
 
 if __name__ == '__main__':
     TeleOpNode()
+
