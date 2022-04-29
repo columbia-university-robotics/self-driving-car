5087ae28a6bf785734afc34bc0d7b3950c09cb0a
NeilNie <yongyang.nie@gmail.com>
2022-04-29 00:42:25 -0400

    add timeout command safety check

--- catkin_ws/src/controls/scripts/inverse_kinematics.py
+++ catkin_ws/src/controls/scripts/inverse_kinematics.py
@@ -2,6 +2,7 @@
 import math
 import rospy
 import numpy as np
+import time
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 from constants import Constants as constants
@@ -25,13 +26,14 @@ def calc_steer_angle(lin_vel, ang_vel, wheel_base_length):
       n = (lin_vel_magnitude * wheel_base_length) / ang_vel_z
       steering_angle = math.atan(n)
   steering_angle = valmap(steering_angle, -math.pi/6, math.pi/6, 0, 1)
-  print("steering angle:", steering_angle)
+  # print("steering angle:", steering_angle)
   return steering_angle
 
 
 class InverseKinematicsNode():
 
   def cmd_callback(self, data):
+    self.callback_time = time.time()
     self.lin_vel_cmd = np.array([data.linear.x, data.linear.y, data.linear.z])
     self.ang_vel_cmd = np.array([data.angular.x, data.angular.y, data.angular.z])
     
@@ -43,15 +45,21 @@ class InverseKinematicsNode():
     self.steer_angle_publisher = rospy.Publisher("systems/output/steer_angle", Float64)
  
     rate = rospy.Rate(30)
-
+    self.callback_time = None
     self.lin_vel_cmd = np.zeros((3, ))
     self.ang_vel_cmd = np.zeros((3, ))
     
     while not rospy.is_shutdown():
       # if self.lin_vel_cmd.any() or self.ang_vel_cmd.any():
         # continue
-      self.rpm_publisher.publish(calc_rpm(self.lin_vel_cmd, constants.radius))
-      self.steer_angle_publisher.publish(calc_steer_angle(self.lin_vel_cmd, self.ang_vel_cmd, constants.l))
+      if self.callback_time is not None and abs(time.time() - self.callback_time) < 1.0:
+         rpm = calc_rpm(self.lin_vel_cmd, constants.radius)
+         steer = calc_steer_angle(self.lin_vel_cmd, self.ang_vel_cmd, constants.l)
+      else:
+         rpm = 0
+         steer = 0.5
+      self.rpm_publisher.publish(rpm)
+      self.steer_angle_publisher.publish(steer)
       rate.sleep()
 
 if __name__ == "__main__":
