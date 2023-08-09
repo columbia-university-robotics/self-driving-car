11d011dea4f5e4c539a384d536271f3e64beccf2
Stephen Petrides <sdpetrides@gmail.com>
2023-08-09 19:12:53 -0400

    Remove scipy dep

--- catkin_ws/src/planning/scripts/a_star_circuit.py
+++ catkin_ws/src/planning/scripts/a_star_circuit.py
@@ -3,12 +3,34 @@ import math
 # import matplotlib.pyplot as plt
 
 import numpy as np
-from scipy.spatial.transform import Rotation
 
 # from PIL import Image, ImageOps
 
 
 show_animation = False
+
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z  # in radians
 
 
 class AStarPlanner:
@@ -327,8 +349,7 @@ def main():
     # start position
     sx = -pose[0]
     sy = -pose[1]
-    rotation = Rotation.from_quat(pose[3:])
-    _, _, yaw = rotation.as_euler("xyz", degrees=False)
+    _, _, yaw = euler_from_quaternion(pose[3:])
     yaw += np.pi / 2
 
     # set goal position
