5e790b9aa65ec1fc71cb0d382104bd6b723b725c
Stephen Petrides <sdpetrides@gmail.com>
2023-08-09 20:17:03 -0400

    Check if dest_dir is directory

--- racing/waypoints.sh
+++ racing/waypoints.sh
@@ -9,6 +9,17 @@ fi
 DOCKER_CONT_ID="$1"
 DEST_DIR="./waypoints_dir"
 
+# Check if DEST_DIR exists and is a directory, create it if not
+if [ ! -d "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+    if [ $? -eq 0 ]; then
+        echo "Created directory $DEST_DIR"
+    else
+        echo "Failed to create directory $DEST_DIR"
+        exit 1
+    fi
+fi
+
 # List of files to copy from the container
 FILES_TO_COPY=(
     "/occupancy_grid.npy"
@@ -28,7 +39,7 @@ for FILE_PATH in "${FILES_TO_COPY[@]}"; do
 done
 
 # Run python script
-python3 a_star_circuit.py "$DEST_DIR"
+python3 ./racing/a_star_circuit.py "$DEST_DIR"
 
 # Copy rx.npy and ry.npy files from DEST_DIR back to the container
 docker cp "$DEST_DIR/rx.npy" "$DOCKER_CONT_ID:/path/in/container"
