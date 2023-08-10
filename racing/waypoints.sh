87232619ec342d6cc0f82c17461036a69b342ccc
Stephen Petrides <sdpetrides@gmail.com>
2023-08-09 20:01:26 -0400

    Provide DEST_DIR as an arg

--- racing/waypoints.sh
+++ racing/waypoints.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Check if the DOCKER_CONT_ID argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 DOCKER_CONT_ID"
+# Check if both DOCKER_CONT_ID and DEST_DIR arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 DOCKER_CONT_ID DEST_DIR"
     exit 1
 fi
 
 DOCKER_CONT_ID="$1"
+DEST_DIR="$2"
 
 # List of files to copy from the container
 FILES_TO_COPY=(
@@ -15,9 +16,6 @@ FILES_TO_COPY=(
     "/map_origin.npy"
     "/pose.npy"
 )
-
-# Destination directory on the host machine
-DEST_DIR="/path/on/host"
 
 # Loop through the list of files and copy them from the container to the host
 for FILE_PATH in "${FILES_TO_COPY[@]}"; do
